837bfcea4f0b8b5e2628ea680461990e5d266c01
Onur Y. Özcan <ozcano@users.noreply.github.com>
2018-11-08 13:50:50 +0300

    input file formatting

--- input_processor.py
+++ input_processor.py
@@ -6,6 +6,8 @@ Created on Tue Oct 23 20:17:52 2018
 
 """
 
+import pandas
+
 assistants = {
         
         "John Doe" : "CEV XXX,Pazartesi,1030,1230;CEV XXX,Salı,1430,1630;CEV XXX,Çarşamba,1330,1530;CEV XXX,Perşembe,930,1230;CEV XXX,Cuma,1330,1730",
