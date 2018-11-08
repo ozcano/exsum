ef667ea58a428ad6d4d91e1c8f826fdd6d76cfe5
Onur Y. Özcan <ozcano@users.noreply.github.com>
2018-11-08 13:10:56 +0300

    Initial commit

---
+++ input_processor.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 23 20:17:52 2018
+
+@author: Onur Y Özcan <http://ozcan.io>
+
+"""
+
+assistants = {
+        
+        "John Doe" : "CEV XXX,Pazartesi,1030,1230;CEV XXX,Salı,1430,1630;CEV XXX,Çarşamba,1330,1530;CEV XXX,Perşembe,930,1230;CEV XXX,Cuma,1330,1730",
+        
+        "Jane Boxman" : "CEV XXX,Salı,1330,1630;CEV XXX,Çarşamba,1030,1230;CEV XXX,Çarşamba,1330,1530;CEV XXX,Cuma,930,1130;CEV XXX,Cuma,1330,1530"
+        
+}
+
