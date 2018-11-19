84123173f5f0d3c4f04f39661c8b0aea903f8e6e
ozcano <onurzcn@gmail.com>
2018-11-19 12:23:37 +0300

    open assistants.txt and append names to list

--- inproc.py
+++ inproc.py
@@ -3,7 +3,6 @@
 @author: Onur Y. Ozcan <http://ozcan.io> '''
 
 # Test cases
-
 t_assistant_names = ('Ali Ozturk','Arzu Hakan','Ayse Gungor','Cem Cesur','Mehmet Celik','Meral Vardar')
 
 t_assistant_schedule = {
@@ -20,11 +19,11 @@ t_assistant_schedule = {
     'Meral Vardar' : 'R,Teaching CEV 202,MON,1130,1330;R,Teaching CEV 202,TUE,930,1030;R,Teaching CEV 202,WED,1030,1230;R,Teaching CEV 202,THU,1330,1530;R,Teaching CEV 202,FRI,930,1230'
 }
 
-print(t_assistant_schedule['Ali Ozturk'])
-
-# using 'with ... as' closes any open files automatically
-# when the code block is done
+assistant_names = []
 filepath = 'input/assistants.txt'
 with open(filepath) as fp:
-    for cnt, line in enumerate(fp):
-        print("Line {} : {}".format(cnt, line))
+    for line in fp:
+        # store assistant names in a separate list
+        if line.startswith("@"):
+            assistant_names.append(line.strip('@\n\t '))
+    print(assistant_names)
